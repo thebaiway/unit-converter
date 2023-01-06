4cba6cf7cc12740d53d3d07a7163a3f835f6c98e
Bailee <bailee.roby@gmail.com>
2023-01-05 23:33:05 -0800

    starting to build interface and adding some js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// how to round decimals 
+// var numb = 349875948759348.30374213;
+// numb = numb.toFixed(2);
+// console.log (numb)
+
+let myUnit = " "
+let title = document.getElementById("title")
+let inputEl = document.getElementById("input-el")
+let convertBtn = document.getElementById("convert-btn")
+
+myUnit.push(inputEl.value)
