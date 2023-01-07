e33b601fe80d1c7c115d030b3a26218f6a890c28
Bailee <bailee.roby@gmail.com>
2023-01-06 16:52:09 -0800

    updating js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,28 @@
-// how to round decimals 
-// var numb = 349875948759348.30374213;
-// numb = numb.toFixed(2);
-// console.log (numb)
-
-let myUnit = " "
 let title = document.getElementById("title")
 let inputEl = document.getElementById("input-el")
 let convertBtn = document.getElementById("convert-btn")
+let length = document.getElementById("length-p")
+let volume = document.getElementById("volume-p")
+let mass = document.getElementById("mass-p")
 
-myUnit.push(inputEl.value)
+
+convertBtn.addEventListener("click", function () {
+    let myUnit = []
+    myUnit.push(inputEl.value)
+
+    let feet = myUnit * 3.281
+    let meters = myUnit / 3.281
+    let gallons = myUnit * 0.264
+    let liters = myUnit / 0.264
+    let lbs = myUnit * 2.204
+    let kilos = myUnit / 2.204
+
+
+    length.innerHTML = `${myUnit} meters = ${feet.toFixed(3)} feet | ${myUnit} feet = ${meters.toFixed(3)} meters`
+
+    volume.innerHTML = `${myUnit} liters = ${gallons.toFixed(3)} gallons | ${myUnit} gallons = ${liters.toFixed(3)} liters`
+
+    mass.innerHTML = `${myUnit} kilos = ${lbs.toFixed(3)} pounds | ${myUnit} pounds = ${kilos.toFixed(3)} kilos`
+
+})
+
